6605cbdbf1cc911daeee12e413191f6066479906
TiKo98 <kogli1998@gmail.com>
2018-02-20 13:59:41 +0100

    New GamePhysics

--- src/project/breakout/controller/BreakoutTimer.java
+++ src/project/breakout/controller/BreakoutTimer.java
@@ -1,29 +1,27 @@
 package project.breakout.controller;
 
+import java.util.TimerTask;
+
 import project.breakout.model.BreakoutModel;
 
-public class BreakoutTimer implements Runnable {
+public class BreakoutTimer extends TimerTask {
 	private long pauseTime;
 	private BreakoutModel model;
-	public boolean go = true;
 
-	public BreakoutTimer(BreakoutModel model, int framesPerSecond) {
+	/**
+	 * @param model
+	 *            The current BreakoutModel.
+	 * @param pauseTime
+	 *            Time between frames in milliseconds.
+	 */
+	public BreakoutTimer(BreakoutModel model, long pauseTime) {
 		this.model = model;
-
-		double pauseTimeDouble = 1.0 / framesPerSecond;
-		pauseTime = (long) (pauseTimeDouble * 1000);
+		this.pauseTime = pauseTime;
 	}
 
 	@Override
 	public void run() {
-		long lastTime = System.currentTimeMillis();
-		while (go) {
-			if (System.currentTimeMillis() >= lastTime + pauseTime)
-			{
-				lastTime = lastTime + pauseTime;
-				model.updateBallsPosition((double) pauseTime / 1000);
-			} 
-		}
-
+		// create new frame
+		model.updateBallsPosition((double) pauseTime / 1000);
 	}
 }
