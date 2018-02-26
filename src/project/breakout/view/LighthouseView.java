59b8b7b95431aca28beab0ca5e987ebc0186b881
cecile <cecile@192.168.2.39>
2018-02-26 15:57:03 +0100

    update

--- src/project/breakout/view/LighthouseView.java
+++ src/project/breakout/view/LighthouseView.java
@@ -11,8 +11,8 @@ import de.cau.infprogoo.lighthouse.LighthouseDisplay;
 public class LighthouseView {
 	private static final String USERNAME = Messages.getString("LighthouseView.0"); //$NON-NLS-1$
 	private static final String PASSWORD = Messages.getString("LighthouseView.1");  //$NON-NLS-1$
-	private static LighthouseDisplay display = new LighthouseDisplay(USERNAME, PASSWORD);
-	private static byte[] data = new byte[14 * 28 * 3];;
+	private static LighthouseDisplay display = new LighthouseDisplay(USERNAME, PASSWORD,25);
+	private static byte[] data = new byte[14 * 28 * 3];
 
 	/**
 	 * Sets up the connection of this class to the lighthouse.
@@ -36,13 +36,34 @@ public class LighthouseView {
 	}
 
 	public static void setBallsPosition(double ballX, double ballY) {
+		// ball size 1*1
 		// TODO This is just a test yet.
+//		byte b = (byte) x;
+//		int c = b & 0xff;
+	
 		for (int i = 0; i < data.length; i += 2) {
-			data[i] = 100;
+			data[i] = (byte) 255;
 		}
 		System.out.println("send test data");
 		updateLighthouseView();
 	}
+	
+	public static void setPaddlePosition(double paddleX, double paddleY) {
+		//data position 365-392
+		// paddle size 8*1
+	}
+	
+	/**
+	 * Removes a brick from the view.
+	 * 
+	 * @param brick
+	 *            The {@code BreakoutBrick} to be removed.
+	 */
+//	public void removeBrick( brick) {
+//		remove(brick);
+//	}
+//	
+	
 
 	/**
 	 * This method updates the lightHouseView and sends the new data-array to it.
