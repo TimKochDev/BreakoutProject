43fefa49ff03392c8686e01b0af792fc3da1d603
TiKo98 <kogli1998@gmail.com>
2018-02-27 18:23:58 +0100

    g

--- src/project/breakout/model/BricksConfig.java
+++ src/project/breakout/model/BricksConfig.java
@@ -22,6 +22,7 @@ import project.breakout.view.BrickType;
  * 
  * 2x ... 
  * 2y...
+>>>>>>> branch 'master' of https://github.com/TiKo98/BreakoutProject
  */
 public class BricksConfig {
 	private static int brickWidth = 40;
