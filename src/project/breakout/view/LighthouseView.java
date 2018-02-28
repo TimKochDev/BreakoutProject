2a230b2b9fa2acbd41ad9c0975ff4afa2edb32b1
cecile <cecile@192.168.2.39>
2018-02-28 18:39:42 +0100

    javadoc

--- src/project/breakout/view/LighthouseView.java
+++ src/project/breakout/view/LighthouseView.java
@@ -62,19 +62,26 @@ public class LighthouseView {
 	}
 
 	public static void setBallsPosition(double ballX, double ballY) {
-		assert ballX >= 0 && ballX <= WINDOWS_PER_FLOOR - BALL_LENGTH: "LighhouseView: ballX < 0 or > WINDOWS_PER_FLOOR - BALL_LENGTH";
+		assert ballX >= 0 && ballX <= WINDOWS_PER_FLOOR
+				- BALL_LENGTH : "LighhouseView: ballX < 0 or > WINDOWS_PER_FLOOR - BALL_LENGTH";
 		assert ballY >= 0 && ballY <= FLOORS - BALL_HEIGHT : "LighhouseView: ballY < 0 or > FLOORS - BALL_HEIGHT";
-			//if (ballX <= WINDOWS_PER_FLOOR - BALL_LENGTH && ballY <= FLOORS - BALL_HEIGHT) {
-				for (int i = 0; i < BALL_LENGTH; i++) {
-					data[(int) (((ballX + i) + ballY * WINDOWS_PER_FLOOR) * RGB)] = (byte) 255;
-				}
+		// if (ballX <= WINDOWS_PER_FLOOR - BALL_LENGTH && ballY <= FLOORS -
+		// BALL_HEIGHT) {
+		for (int i = 0; i < BALL_LENGTH; i++) {
+			data[(int) (((ballX + i) + ballY * WINDOWS_PER_FLOOR) * RGB)] = (byte) 255;
+		}
 		updateLighthouseView();
 	}
 
+	/**
+	 * 
+	 * @param paddleX
+	 * @param paddleY
+	 */
 	public static void setPaddlePosition(double paddleX, double paddleY) {
-		assert paddleX <= WINDOWS_PER_FLOOR - BALL_LENGTH: "LighhouseView: paddleX < 0";
-		assert paddleY == FLOORS-1: "LighhouseView: paddleY != FLOORS";
-		
+		assert paddleX <= WINDOWS_PER_FLOOR - BALL_LENGTH : "LighhouseView: paddleX < 0";
+		assert paddleY == FLOORS - 1 : "LighhouseView: paddleY != FLOORS";
+
 		// red
 		for (int i = 0; i < PADDEL_LENGTH; i++) {
 			data[(int) (((paddleX + i) + paddleY * WINDOWS_PER_FLOOR) * RGB)] = (byte) 255;
